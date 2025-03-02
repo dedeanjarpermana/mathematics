800690b2c07db18a702dc04a7741f6ab4cb85d49
dede anjar permana <dedeanjarpermana@gmail.com>
2025-03-03 06:01:17 +0700

    Update code to test database railway

--- db.js
+++ db.js
@@ -1,23 +1,27 @@
 // const mysql = require('mysql2');
-const dotenv = require('dotenv');
-// const mysql = require("mysql2/promise"); // Gunakan versi promise
-dotenv.config();
+
 
 const mysql = require("mysql2/promise"); // Gunakan versi promise
 
 
 
-// Buat koneksi pool
+require("dotenv").config();
+
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306, // Gunakan default 3306 jika tidak ada
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
+// Cek koneksi saat aplikasi mulai
+db.getConnection()
+  .then(() => console.log("✅ Database Connected!"))
+  .catch((err) => console.error("❌ Database Connection Failed:", err));
 
 
 
