6edaf3d5a9d591486ed04ec4297f88cf07ea990b
dede anjar permana <dedeanjarpermana@gmail.com>
2025-03-04 09:17:04 +0700

    Update code to test database railway

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
-// const mysql = require('mysql2');
+const mysql = require('mysql2');
 
 
-const mysql = require("mysql2/promise"); // Gunakan versi promise
+// const mysql = require("mysql2/promise"); // Gunakan versi promise
 require("dotenv").config();
 
 const db = mysql.createPool({
